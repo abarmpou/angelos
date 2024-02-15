2efeafce5ef3e542ed0b94d60e8588049301ccd4
Angelos Barmpoutis <angelbar@ufl.edu>
2024-02-14 23:32:12 -0500

    links corrected

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -1,14 +1,14 @@
-<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="https://archipelago.phrasewise.com/rsd">
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://research.dwi.ufl.edu/projects/angelos</homePageLink>
+		<homePageLink>https://abarmpou.github.io/angelos</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://research.dwi.ufl.edu/projects/angelos/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://research.dwi.ufl.edu/projects/angelos/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://research.dwi.ufl.edu/projects/angelos/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://research.dwi.ufl.edu/projects/angelos/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://research.dwi.ufl.edu/projects/angelos/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://abarmpou.github.io/angelos/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://abarmpou.github.io/angelos/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://abarmpou.github.io/angelos/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://abarmpou.github.io/angelos/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://abarmpou.github.io/angelos/wp-json/" />
 			</apis>
 	</service>
 </rsd>
